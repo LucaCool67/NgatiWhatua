fe933c0006d5777176be7db0f2f448febfb4b988
LucaCool67 <lucaroe829@gmail.com>
2026-06-24 14:57:13 +1200

    added more JS to FIX at home and changes

--- ngati.js
+++ ngati.js
@@ -1,9 +1,15 @@
 window.onscroll = function() {scrollFunction()};
 
+let scrollValue = 30
+
 function scrollFunction() {
   if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-    document.querySelector("nav").style.top = "0";
+    document.getElementById("nav").style.top = "0";
   } else {
-    document.querySelector("nav").style.top = "-30vh";
+    for(let i = 0; i > scrollValue; i++){
+        setTimeout(() => {
+            document.getElementById("nav").style.top -= "1vh";
+        }, 1000)
+    }
   }
 }
