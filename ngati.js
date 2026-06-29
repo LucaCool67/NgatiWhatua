81612254849cccecfd13f204a9e3c7e1dba361c4
LucaCool67 <lucaroe829@gmail.com>
2026-06-29 12:16:44 +1200

    Added images, changed js, css and html file and 4 changes overall

--- ngati.js
+++ ngati.js
@@ -5,11 +5,13 @@ let scrollValue = 30
 function scrollFunction() {
   if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
     document.getElementById("nav").style.top = "0";
-  } else {
+    document.getElementById("nav").style.opacity = 100;
+  }
+  else {
     for(let i = 0; i > scrollValue; i++){
-        setTimeout(() => {
-            document.getElementById("nav").style.top -= "1vh";
-        }, 1000)
+      document.getElementById("nav").style.top -= "30vh";
+      document.getElementById("nav").style.visibility = 0;
     }
+    
   }
 }
